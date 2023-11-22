431e011f583c448b72f29d08873c4a61742d9c0c
unknown <hebrusgraphics@gmail.com>
2023-11-21 23:55:53 -0800

    little changes

--- Backend/app.py
+++ Backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, session
 from flask_migrate import Migrate
 from model.model import Student,db
 from flask_cors import CORS, cross_origin
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/orcs_db'
@@ -11,21 +12,28 @@ app.secret_key = 'your_secret_key'  # Set your secret key for sessions
 
 CORS(app, supports_credentials=True)  # Enable CORS for all routes
 
+bycrpt = Bcrypt(app)
 
 migrate = Migrate(app, db)
 
 
+# login student
 @app.route('/api/users', methods=['POST'])
-def create_user():
+def student_login():
     try:
         if request.method == 'POST':
             data = request.get_json()
-            new_user = Student(matric_no=data.get('matric_no'), password=data.get('password'))
-
+            password = data.get('password')
+            hash_pass = bycrpt.generate_password_hash(password)
+            new_user = Student(matric_no=data.get('matric_no'), password=hash_pass)
             db.session.add(new_user)
             db.session.commit()
+            # start a session
+            session['stud_mat'] = data.get('matric_no')
+            session['logged_in'] = True
 
-            return jsonify({'message': 'User created successfully'}),201
+
+            return jsonify({'message': 'User created successfully', data:new_user}),201
         
     except Exception as e:
         return jsonify({'error': str(e)}), 500
